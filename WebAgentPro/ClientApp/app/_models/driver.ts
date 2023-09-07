3ed65e614aed82ca595b65032560dcc4fb5ead59
Jared Howard <jared.howard@avanade.com>
2023-09-07 15:59:30 -0700

    Added driver, quote, and vehicle classes

---
+++ WebAgentPro/ClientApp/app/_models/driver.ts
@@ -0,0 +1,18 @@
+﻿import { Vehicle } from "./vehicle";
+
+export class Driver {
+    driverId: number;
+    driverName: string;
+    driverSSN: string;
+    driverLicenseNumber: string;
+    driverLicenseState: string;
+    driverDOB: Date;
+    safeDrivingSchool: boolean;
+    quoteMultiplier: number;
+    quoteId: number;
+    vehicles: Vehicle[];
+
+    constructor() {
+        this.vehicles = [];
+    }
+}
