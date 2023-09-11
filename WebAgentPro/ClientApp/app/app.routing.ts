4e7d20d8dd4a8d0f88d2add4ab265eaffb7db8c5
Jared Howard <jared.howard@avanade.com>
2023-09-11 10:53:53 -0700

    Component/scaffolding added for adding vehicle info 2

--- WebAgentPro/ClientApp/app/app.routing.ts
+++ WebAgentPro/ClientApp/app/app.routing.ts
@@ -11,11 +11,8 @@ import { CustomerInfo1Component } from './quotemanagement/customer-info1/custome
 import { CustomerInfo2Component } from './quotemanagement/customer-info2/customer-info2.component';
 import { DriverInfoComponent } from './quotemanagement/driver-info/driver-info.component';
 import { VehicleInfo1Component } from './quotemanagement/vehicle-info1/vehicle-info1.component';
-
-/* Related to routing for the following tasks:
- * 9275
 import { VehicleInfo2Component } from './quotemanagement/vehicle-info2/vehicle-info2.component';
-*/
+
 import { RoutenotfoundComponent } from './_security/routenotfound/routenotfound.component';
 
 const appRoutes: Routes = [
@@ -27,6 +24,7 @@ const appRoutes: Routes = [
     { path: 'quotes2', component: CustomerInfo2Component },
     { path: 'quotes3', component: DriverInfoComponent },
     { path: 'quotes4', component: VehicleInfo1Component },
+    { path: 'quotes5', component: VehicleInfo2Component },
 
     // parameterized route so you can tell the component what action you want to take on which state's discounts
     { path: 'discount/:action/:state', component: DiscountEditComponent, canActivate: [AuthGuard], data: { roles: [Roles.Manager] } },
