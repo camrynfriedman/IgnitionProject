a6ca66fff3b0b2733e15d6dc38a29127a80f3528
Camryn Friedman <camryn.friedman@avanade.com>
2023-09-12 09:51:17 -0400

    Updated Quote dto, mapper, service. Added driver repo to call from Quote service.

---
+++ WebAgentPro/Api/Repositories/DriverRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAgentPro.Api.Models;
+using WebAgentPro.Data;
+
+namespace WebAgentPro.Api.Repositories
+{
+    /*
+     * Contains the driverRepo interface as well as the driverRepo object class.
+     */
+public interface IDriverRepository
+    {
+        // Get all drivers
+        Task<List<Driver>> GetAllDrivers();
+
+        // get by ID
+        Task<Driver> GetDriver(int driverID);
+
+        // edit driver
+        Task EditDriver(Driver d);
+
+        // insert
+        Task AddDriver(Driver d);
+
+        // delete
+        Task RemoveDriver(int driverID);
+
+  
+
+    }
+    public class DriverRepository : IDriverRepository
+    {
+        //connection to database context
+        private readonly WapDbContext _context;
+
+
+        public DriverRepository(WapDbContext context)
+        {
+            _context = context;
+
+        }
+
+        //Implement methods
+        public async Task<List<Driver>> GetAllDrivers()
+        {
+            var drivers = await _context.Drivers.ToListAsync();
+
+            return drivers;
+        }
+
+        public async Task<Driver> GetDriver(int driverID)
+        {
+            Driver driver = await _context.Drivers.Where(d => d.DriverId == driverID).FirstOrDefaultAsync();
+            return driver;
+        }
+
+        public async Task EditDriver(Driver d)
+        {
+            _context.Entry(d).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddDriver(Driver d)
+        {
+            _context.Drivers.Add(d);
+            await _context.SaveChangesAsync();
+        }
+
+        //removes driver by driverID
+        public async Task RemoveDriver(int driverID)
+        {
+            _context.Drivers.Remove(_context.Drivers.Where(d=> d.DriverId == driverID).FirstOrDefault());
+            await _context.SaveChangesAsync();
+        }
+    }
+    
+}
--- WebAgentPro/Api/Services/QuoteService.cs
+++ WebAgentPro/Api/Services/QuoteService.cs
@@ -12,11 +12,7 @@ namespace WebAgentPro.Api.Services
      * and calls methods from the repository.
      * 
      * 
-     * needs to use vehicle repo and driver repo to create those objects before creating final quote
-     * 
-     * loop over each driver, create drivers
-     * loop over each vehicles, create vehicles
-     * construct quote object using those IDs generated
+     *
      * */
 
     public interface IQuoteService
@@ -34,15 +30,21 @@ namespace WebAgentPro.Api.Services
         private readonly IQuoteRepository _quoteRepo;
 
         //add Vehicle and Driver repos
+        private readonly IVehicleRepository _vehicleRepo;
+
+        private readonly IDriverRepository _driverRepo;
 
         //use mapper
         private QuoteMapper map;
 
 
         //constructor
-        public QuoteService(IQuoteRepository quoteRepo)
+        public QuoteService(IQuoteRepository quoteRepo, IVehicleRepository vehicleRepo, IDriverRepository driverRepo)
         {
             _quoteRepo = quoteRepo;
+            _vehicleRepo = vehicleRepo;
+            _driverRepo = driverRepo;
+
             map = new QuoteMapper();
         }
         public async Task<List<QuoteDto>> GetAllQuotes()
@@ -74,8 +76,21 @@ namespace WebAgentPro.Api.Services
             await _quoteRepo.EditQuote(map.DtoToQuote(q));
         }
 
+        // needs to use vehicle repo and driver repo to create those objects before creating final quote
+        // use DTOs?
+     /*
+     * loop over each driver, create drivers
+     * loop over each vehicles, create vehicles
+     * construct quote object using those IDs generated*/
         public async Task AddQuote(QuoteDto q)
         {
+            List<Driver> drivers = new List<Driver>();
+            List<Vehicle> vehicles = new List<Vehicle>();
+            //figure out how to know how many drivers and vehicles to add
+            //create a list of drivers and vehicles and populate using AddDriver and AddVehicle
+            //get the IDs of each driver and add to Quote
+            //get the IDs of each vehicle and add to Quote
+
             if (_quoteRepo.GetQuote(q.QuoteId) != null)
             {
                 throw new Exception("Quote already exists.");
